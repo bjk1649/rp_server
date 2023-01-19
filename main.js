458f4753c59ffc650144cc67b4d89e2407a3aa61
bjk1649 <qhwns1649@naver.com>
2023-01-19 19:52:54 +0900

    feat: 기본 파일들 추가

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 //express 서버 셋업
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//express 서버 실행
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+//express 서버에 라우팅 설정
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+//라우팅 설정
+app.get('/test', (req, res) => {
+    res.send('test')
+})
