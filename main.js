2357e079c082a0d0a475f635075e86f910be09c3
bjk1649 <qhwns1649@naver.com>
2023-01-19 20:10:09 +0900

    feat: 카카오 로그인 기능 추가

--- main.js
+++ main.js
@@ -8,12 +8,15 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
-//express 서버에 라우팅 설정
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get('/callbacks/kakao/sign_in', async (request, response) => {
+    //Authentication Code 받아 돌려줄 api 
+  const redirect = `webauthcallback://success?${new URLSearchParams(request.query).toString()}`;
+  console.log(`Redirecting to ${redirect}`);
+  response.redirect(307, redirect);
+  })
 
-//라우팅 설정
-app.get('/test', (req, res) => {
-    res.send('test')
-})
+app.post('/callbacks/kakao/token', async (request, response) => {
+      //발급 받은 kakao AccessCode로 사용자 확인후 firebase 로 custom token 생성하기 위한 api
+  kakao_auth.createFirebaseToken(request.body["accessToken"],(resulst)=>{
+    response.send(resulst);})
+  })
